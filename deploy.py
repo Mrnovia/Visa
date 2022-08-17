9fafe0171b353889121e7bd9d1baa7ac7b1db97f
nelson novia <fenome14@gmail.com>
2022-08-17 19:25:41 +0000

    version 2

--- deploy.py
+++ deploy.py
@@ -1,4 +1,6 @@
 #Deploying a python web application
-#echo "Hello world :wq!= shell script "echo"
-python ("hello world") = python script
+#echo "Hello world" = shell script "echo"
+#python ("hello world") = python script
 #python script
+
+print('Hello world')
